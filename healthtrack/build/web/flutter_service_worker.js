6484f5dc1db82f16224d80b4c2bbacc851f9fddc
Edwin Malunoc <edwin@example.com>
2026-06-11 10:21:10 +0800

    Deploy: rebuild Flutter web --release (June 11 2026)

--- healthtrack/build/web/flutter_service_worker.js
+++ healthtrack/build/web/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f1014ea5de65efdd7542d345449cda52
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "a19e44f75978fc04f5c6181ae9fd9d0a",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d6141b2c5de722bd588afa7414e45565",
+"flutter_bootstrap.js": "f0a9878dfb068986f05f2ae9f1415fd6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
